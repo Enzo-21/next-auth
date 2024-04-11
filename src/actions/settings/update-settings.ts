25b40a8374a3ac14e7dd6b19a7ecdb9a133fbe0f
Enzo-21 <enzo21barrera@gmail.com>
2024-04-11 20:21:24 -0300

    Added route endpoints for mobile app

--- src/actions/settings/update-settings.ts
+++ src/actions/settings/update-settings.ts
@@ -8,90 +8,94 @@ import { TokensService } from "@/services/tokens-service"
 import { UserService } from "@/services/user-service"
 import { z } from "zod"
 import bcrypt from 'bcryptjs'
+import { NextApiRequest } from "next"
 
 export const updateSettings = async (
-    values: z.infer<typeof SettingsSchema>
-) => {    
-    
+    values: z.infer<typeof SettingsSchema>,
+    req?: Request
+) => {
 
-    // Check if there's a user in the session
-    const user = await currentUser()
-    if (!user?.id) {
-        return { error: 'Unauthorized' }
-    }
+    try {
 
-    // Check if that user exists in the db
-    const userFromDb = await UserService.getUserById(user.id)
-    if (!userFromDb) {
-        return { error: 'Unauthorized' }
-    }
-
-    // Prevent users registered with oauth providers from changing their emails
-    if (user.isOAuth) {
-        values.email = userFromDb.email
-    }
-
-    // Updating an email
-    if (values.email && values.email !== userFromDb.email) {
-        const existingUser = await UserService.getUserByEmail(values.email)
-
-        if (existingUser) {
-            return { error: 'Email already in use' }
+        // Check if there's a user in the session
+        const user = await currentUser(req)
+        if (!user?.id) {
+            return { error: 'Unauthorized' }
         }
 
-        const verificationToken = await TokensService.generateVerificationToken(values.email)
-
-
-        if (verificationToken) {
-            await sendVerificationEmail(user.name ,verificationToken.email, verificationToken.token)
-        } else {
-            return { error: 'An unexpected error occurred' }
+        // Check if that user exists in the db
+        const userFromDb = await UserService.getUserById(user.id)
+        if (!userFromDb) {
+            return { error: 'Unauthorized' }
         }
 
-        return { success: 'Verification email sent' }
-
-
-    }
-
-    // Creating password
-    if (!values.hasCredentials && values.newPassword) {
-        if (userFromDb.password) {
-            return {error: 'You already have a password, you can not create a new one'}
+        // Prevent users registered with oauth providers from changing their emails
+        if (user.isOAuth) {
+            values.email = userFromDb.email
         }
 
-        const hashedPassword = await bcrypt.hash(values.newPassword, 10)
-        values.password = hashedPassword
-        values.newPassword = undefined
+        // Updating an email
+        if (values.email && values.email !== userFromDb.email) {
+            const existingUser = await UserService.getUserByEmail(values.email)
 
-    }
-
-    // Updating password
-    if (values.password && values.newPassword) {
-        if (userFromDb.password) {
-            const passwordsMatch = await bcrypt.compare(
-                values.password,
-                userFromDb.password
-            )
-
-            if (!passwordsMatch) {
-                return {error: 'Incorrect password'}
+            if (existingUser) {
+                return { error: 'Email already in use' }
             }
-        } 
 
-        const hashedPassword = await bcrypt.hash(values.newPassword, 10)
-        values.password = hashedPassword
-        values.newPassword = undefined
-      
+            const verificationToken = await TokensService.generateVerificationToken(values.email)
+
+
+            if (verificationToken) {
+                await sendVerificationEmail(user.name, verificationToken.email, verificationToken.token)
+            } else {
+                return { error: 'An unexpected error occurred' }
+            }
+
+            return { success: 'Verification email sent' }
+
+
+        }
+
+        // Creating password
+        if (!values.hasCredentials && values.newPassword) {
+            if (userFromDb.password) {
+                return { error: 'You already have a password, you can not create a new one' }
+            }
+
+            const hashedPassword = await bcrypt.hash(values.newPassword, 10)
+            values.password = hashedPassword
+            values.newPassword = undefined
+
+        }
+
+        // Updating password
+        if (values.password && values.newPassword) {
+            if (userFromDb.password) {
+                const passwordsMatch = await bcrypt.compare(
+                    values.password,
+                    userFromDb.password
+                )
+
+                if (!passwordsMatch) {
+                    return { error: 'Incorrect password' }
+                }
+            }
+
+            const hashedPassword = await bcrypt.hash(values.newPassword, 10)
+            values.password = hashedPassword
+            values.newPassword = undefined
+
+        }
+
+        values.hasCredentials = undefined
+
+        await db.user.update({
+            where: { id: userFromDb.id },
+            data: { ...values }
+        })
+
+        return { success: 'Settings updated' }
+    } catch (error: any) {
+        return  { error: error?.message }
     }
-
-  values.hasCredentials = undefined
-
-    await db.user.update({
-        where: { id: userFromDb.id },
-        data: { ...values }
-    })
-
-    return { success: 'Settings updated' }
-
-
 }
--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -1,19 +1,17 @@
+import { currentUser } from "@/lib/auth-user";
 import { TokensService } from "@/services/tokens-service";
 import { UserService } from "@/services/user-service";
+import { NextApiRequest } from "next";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextApiRequest) {
 
-    const headersList = headers()
-    const bearerToken = headersList.get('authorization')
-    const decoded = await TokensService.verifyAuthToken(bearerToken)
+    const user = await currentUser(req)
 
-    if (!decoded) {
+    if (!user) {
         return new NextResponse(null, { status: 404 })
     }
 
-    const user = await UserService.getUserById(decoded.userId as string)
-    
     return new NextResponse(JSON.stringify(user), { status: 200 })
 }
